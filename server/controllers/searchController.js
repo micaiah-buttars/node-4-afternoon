4fe691625893abcdb78547b8cda53763d000c754
Micaiah Buttars <michael.buttars@gmail.com>
2019-04-02 14:51:11 -0600

    finished

---
+++ server/controllers/searchController.js
@@ -0,0 +1,13 @@
+const swag = require('../models/swag')
+
+module.exports = {
+    search: (req, res) => {
+        const {category} = req.query
+        if(!category){
+            res.status(200).send(swag)
+        } else{
+            let filteredSwag = swag.filter(item => item.category === category)
+            res.status(200).send(filteredSwag)
+        }
+    }
+}
